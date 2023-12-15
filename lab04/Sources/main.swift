e5dad52b8035e85813d78200b66f27913a4c29a9
killerror <rmurzaliev@yandex.ru>
2023-12-16 02:24:15 +0400

    lab04

---
+++ lab04/Sources/main.swift
@@ -0,0 +1,49 @@
+enum PizzaType: String {
+    case margherita
+    case pepperoni
+    case vegetarian
+    case hawaiian
+    case barbecueChicken
+}
+
+struct Pizza {
+    enum Toppings: String {
+        case pepperoni
+        case tomatoes
+        case cheese
+    }
+
+    let cost: Double
+    let pizzaType: PizzaType
+    let crustType: String // "толстое" или "тонкое"
+    let toppings: [Toppings]
+}
+
+class Pizzeria {
+    private var pizzas: [Pizza]
+
+    init(initialPizzas: [Pizza]) {
+        self.pizzas = initialPizzas
+    }
+
+    func addPizza(newPizza: Pizza) {
+        pizzas.append(newPizza)
+    }
+
+    func getAllPizzas() -> [Pizza] {
+        return pizzas
+    }
+}
+
+let initialPizzas = [Pizza(cost: 10.99, pizzaType: .margherita, crustType: "тонкое", toppings: [.cheese]),
+                     Pizza(cost: 12.99, pizzaType: .pepperoni, crustType: "толстое", toppings: [.cheese, .pepperoni])]
+
+let pizzeria = Pizzeria(initialPizzas: initialPizzas)
+
+let newPizza = Pizza(cost: 14.99, pizzaType: .vegetarian, crustType: "тонкое", toppings: [.cheese, .tomatoes])
+pizzeria.addPizza(newPizza: newPizza)
+
+let allPizzas = pizzeria.getAllPizzas()
+for pizza in allPizzas {
+    print("Pizza Type: \(pizza.pizzaType.rawValue), Cost: \(pizza.cost), Crust: \(pizza.crustType), Toppings: \(pizza.toppings.map { $0.rawValue })")
+}
