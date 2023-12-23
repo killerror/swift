728c59781aa17c6a48fb8ff34d1a1984c1dfc3f8
killerror <rmurzaliev@yandex.ru>
2023-12-24 02:13:12 +0400

    fix lab04

--- lab04/Sources/main.swift
+++ lab04/Sources/main.swift
@@ -1,12 +1,13 @@
-enum PizzaType: String {
-    case margherita
-    case pepperoni
-    case vegetarian
-    case hawaiian
-    case barbecueChicken
-}
 
 struct Pizza {
+    enum PizzaType: String {
+        case margherita
+        case pepperoni
+        case vegetarian
+        case hawaiian
+        case barbecueChicken
+    }
+    
     enum Toppings: String {
         case pepperoni
         case tomatoes
@@ -14,7 +15,7 @@ struct Pizza {
     }
 
     let cost: Double
-    let pizzaType: PizzaType
+    let pizzaType: [PizzaType]
     let crustType: String // "толстое" или "тонкое"
     let toppings: [Toppings]
 }
@@ -23,7 +24,7 @@ class Pizzeria {
     private var pizzas: [Pizza]
 
     init(initialPizzas: [Pizza]) {
-        self.pizzas = initialPizzas
+        pizzas = initialPizzas
     }
 
     func addPizza(newPizza: Pizza) {
@@ -31,19 +32,19 @@ class Pizzeria {
     }
 
     func getAllPizzas() -> [Pizza] {
-        return pizzas
+        pizzas
     }
 }
 
-let initialPizzas = [Pizza(cost: 10.99, pizzaType: .margherita, crustType: "тонкое", toppings: [.cheese]),
-                     Pizza(cost: 12.99, pizzaType: .pepperoni, crustType: "толстое", toppings: [.cheese, .pepperoni])]
+let initialPizzas = [Pizza(cost: 10.99, pizzaType: [.margherita], crustType: "тонкое", toppings: [.cheese]),
+                     Pizza(cost: 12.99, pizzaType: [.pepperoni], crustType: "толстое", toppings: [.cheese, .pepperoni])]
 
 let pizzeria = Pizzeria(initialPizzas: initialPizzas)
 
-let newPizza = Pizza(cost: 14.99, pizzaType: .vegetarian, crustType: "тонкое", toppings: [.cheese, .tomatoes])
+let newPizza = Pizza(cost: 14.99, pizzaType: [.vegetarian], crustType: "тонкое", toppings: [.cheese, .tomatoes])
 pizzeria.addPizza(newPizza: newPizza)
 
 let allPizzas = pizzeria.getAllPizzas()
 for pizza in allPizzas {
-    print("Pizza Type: \(pizza.pizzaType.rawValue), Cost: \(pizza.cost), Crust: \(pizza.crustType), Toppings: \(pizza.toppings.map { $0.rawValue })")
+    print("Pizza Type: \(pizza.pizzaType.map { $0.rawValue }), Cost: \(pizza.cost), Crust: \(pizza.crustType), Toppings: \(pizza.toppings.map { $0.rawValue })")
 }
